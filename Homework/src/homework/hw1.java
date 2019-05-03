d082f80b30dfbcae0463498fe3bce14a75b0eaf4
HP <HP@121.162.57.119>
2019-05-03 23:08:08 +0900

    Done

--- Homework/src/homework/hw1.java
+++ Homework/src/homework/hw1.java
@@ -1,5 +1,14 @@
 package homework;
 
 public class hw1 {
-
+	
+	public static void main(String[] args) {
+		
+		for(int i = 2; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				System.out.println(i + " * " + j + " = " + i*j);
+			}
+		}
+		
+	}
 }
